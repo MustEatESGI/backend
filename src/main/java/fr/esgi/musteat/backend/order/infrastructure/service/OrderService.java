5d48521c030d980a4b70b243c9f3fc8e633035fa
Quentin ROUVILLE <qrouville@posteo.net>
2022-06-12 21:53:45 +0200

    Add route for trending restaurants

--- src/main/java/fr/esgi/musteat/backend/order/infrastructure/service/OrderService.java
+++ src/main/java/fr/esgi/musteat/backend/order/infrastructure/service/OrderService.java
@@ -4,11 +4,18 @@ import fr.esgi.musteat.backend.kernel.Service;
 import fr.esgi.musteat.backend.kernel.Validator;
 import fr.esgi.musteat.backend.order.domain.Order;
 import fr.esgi.musteat.backend.order.domain.OrderRepository;
+import fr.esgi.musteat.backend.restaurant.domain.Restaurant;
 
 @org.springframework.stereotype.Service
 public class OrderService extends Service<OrderRepository, Order, Long> {
 
     public OrderService(OrderRepository repository, Validator<Order> validator) {
         super(repository, validator, "order");
+    }
+
+    public long getNumberOfOrdersForRestaurant(Restaurant restaurant) {
+        return this.getAll().stream()
+                .filter(order -> order.getRestaurant().equals(restaurant))
+                .count();
     }
 }
